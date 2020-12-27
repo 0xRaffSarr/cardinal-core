8d1c313ba6de214c48444d79330f7c4aae6d6f40
Raffaele Sarracino <raffaele56t@gmail.com>
2020-12-27 15:41:25 +0100

    Added new Cardinal exception

---
+++ CardinalCore/Exception/InvalidArgumentException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (c) 2020. Cardinal
+ * Author: Raffaele Sarracino - https://raffaelesarracino.it
+ */
+
+namespace CardinalCore\Exception;
+
+
+class InvalidArgumentException extends \CardinalCore\Exception\Exception
+{
+
+}
