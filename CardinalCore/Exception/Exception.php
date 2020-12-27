ef4714a450cec840202f9dda9b5f3395eb604629
Raffaele Sarracino <raffaele56t@gmail.com>
2020-12-27 15:32:08 +0100

    Init Cardinal Exception

---
+++ CardinalCore/Exception/Exception.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright (c) 2020. Cardinal
+ * Author: Raffaele Sarracino - https://raffaelesarracino.it
+ */
+
+namespace CardinalCore\Exception;
+
+class Exception extends \Exception
+{
+
+}
