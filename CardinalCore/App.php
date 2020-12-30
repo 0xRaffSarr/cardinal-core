c76b64193903ce92676dad493df4c8e9defb5146
Raffaele Sarracino <raffaele56t@gmail.com>
2020-12-30 16:14:52 +0100

    New App instance load the project paths

--- CardinalCore/App.php
+++ CardinalCore/App.php
@@ -9,6 +9,7 @@ namespace CardinalCore;
 
 use CardinalCore\Http\Request;
 use CardinalCore\Routing\Routing;
+use mysql_xdevapi\Result;
 
 class App
 {
@@ -18,6 +19,7 @@ class App
 
     protected $appURL;
 
+    protected $paths = [];
     /**
      * @var Request
      */
@@ -37,6 +39,10 @@ class App
         $this->currentRequest = Request::createFromGlobals();
         //inizializzazione routing
         $this->routing = Routing::create($this->currentRequest);
+        /*
+         * Load the system dir from the root of the project
+         */
+        $this->loadPaths();
     }
 
     /**
@@ -56,6 +62,70 @@ class App
      */
     public function routing() {
         return $this->routing;
+    }
+
+    /**
+     * Return all system path if key is null or a specific path. If path not exists return null.
+     *
+     * @return array
+     */
+    public function paths(string $key = null) {
+        if(is_null($key)) {
+            return $this->paths;
+        }
+
+        $keyList = explode('.', $key);
+        $tmpArr = $this->paths;
+
+        foreach ($keyList as $key) {
+            if(array_key_exists($key, $tmpArr)) {
+                $tmpArr = $tmpArr[$key];
+            }
+            else {
+                return null;
+            }
+        }
+
+        return $tmpArr;
+    }
+
+    /**
+     * Load the system paths
+     */
+    private function loadPaths() {
+        $appRoot = realpath(dirname((new \ReflectionClass($this))->getFileName()).'/..');
+        $sourcePath = realpath($appRoot.'/src');
+
+        $this->paths = [
+            'app_root' => $appRoot,
+            'source_path' => [
+                'root' => $sourcePath,
+            ]
+        ];
+
+        $this->paths['source_path'] = array_merge($this->paths['source_path'], $this->findDir($this->paths['source_path']['root']));
+    }
+
+    /**
+     * Recursive scan of the path and find dir
+     *
+     * @param $path
+     * @return array
+     */
+    private function findDir($path) {
+        $dir = [];
+        $scandir = array_diff(scandir($path), ['.', '..']);
+
+        foreach ($scandir as $d) {
+            $dirRealPath = realpath($path.'/'.$d);
+
+            if(is_dir($dirRealPath)) {
+                $dir[strtolower($d)]['root'] = $dirRealPath;
+                $dir[strtolower($d)] = array_merge($dir[strtolower($d)], $this->findDir($dirRealPath));
+            }
+        }
+
+        return $dir;
     }
 
     /**
