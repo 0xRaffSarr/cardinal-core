5e144a284669df63280b60b041b285ee98a7071b
Raffaele Sarracino <raffaele56t@gmail.com>
2020-12-31 13:50:29 +0100

    Added Provider interface to Kernel module

---
+++ CardinalCore/Kernel/Contracts/Provider.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (c) 2020. Cardinal
+ * Author: Raffaele Sarracino - https://raffaelesarracino.it
+ */
+
+namespace CardinalCore\Kernel\Contracts;
+
+
+interface Provider
+{
+    public function boot();
+}
