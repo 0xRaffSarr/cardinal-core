98002e96d178b0d56ebe398c83778d9dcefc7c70
Raffaele Sarracino <raffaele56t@gmail.com>
2021-01-22 21:45:47 +0100

    Initialized model actions trait

---
+++ CardinalCore/Database/Traits/ModelActions.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright (c) 2021. Cardinal
+ * Author: Raffaele Sarracino - https://raffaelesarracino.it
+ */
+
+namespace CardinalCore\Database\Traits;
+
+trait ModelActions
+{
+    /**
+     * Delete the model information
+     */
+    public function delete () {
+        //TODO: Implement delete methods
+    }
+
+    /**
+     * Create the model object
+     *
+     * @param array $values
+     */
+    public function create(array $values) {
+        //TODO: implement create method
+    }
+
+    public function update(array $values) {
+        //TODO: implement update method
+    }
+
+    public function save(): bool {
+        //TODO: implement save method
+    }
+}
