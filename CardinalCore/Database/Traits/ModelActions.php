81f76c08806508f8b80afacd39056fd79cb99f7f
Raffaele Sarracino <raffaele56t@gmail.com>
2021-01-27 13:31:49 +0100

    Implemented the update method and added method to set the attribute value

--- CardinalCore/Database/Traits/ModelActions.php
+++ CardinalCore/Database/Traits/ModelActions.php
@@ -7,6 +7,7 @@
 namespace CardinalCore\Database\Traits;
 
 use CardinalCore\Database\Database;
+use CardinalCore\Database\Exception\DatabaseException;
 use CardinalCore\Database\Model;
 
 trait ModelActions
@@ -24,37 +25,58 @@ trait ModelActions
     /**
      * Create the model object
      *
-     * @param array $values The values of the array.
+     * @param array $values The values of the model.
      *
      * @param bool $save Determines whether to save the model in the database. If save is set to true, the model is
      *                   saved in the database, otherwise it is not saved in database.
      *                   By default, save is set to true.
      *
      * @return Model     Return an instance of model.
+     *
+     * @throws DatabaseException
      */
     public static function create(array $values, bool $save = true) {
         $modelObj = new static();
 
-        foreach ($modelObj->getModelAttributes() as $attribute) {
-            //check that the model fillable attributes, exists in values array
-            if(array_key_exists($attribute, $values)) {
-                $modelObj->{$attribute} = $values[$attribute];
-            }
-        }
+        $modelObj->setAttributes($values);
 
         //If the saved is set to true, the model is saved in databse
         if($save) {
-            $modelObj->save();
+            if(!$modelObj->save()) {
+                throw new DatabaseException('Cannot save the data');
+            }
         }
 
         return $modelObj;
     }
 
+    /**
+     * Updates the model information and saves it in the database
+     *
+     * @param array $values The values of the model.
+     * @return bool
+     */
     public function update(array $values) {
-        //TODO: implement update method
+        $this->setAttributes($values);
+
+        return $this->save();
     }
 
     public function save(): bool {
         //TODO: implement save method
+    }
+
+    /**
+     * Set the model attributes value
+     *
+     * @param array $values
+     */
+    protected function setAttributes(array $values) {
+        foreach ($this->getModelAttributes() as $attribute) {
+            //check that the model fillable attributes, exists in values array
+            if(array_key_exists($attribute, $values)) {
+                $this->{$attribute} = $values[$attribute];
+            }
+        }
     }
 }
